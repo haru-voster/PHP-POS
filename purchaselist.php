06ae5c55be5415b0a4db70c2c2cb7a14b528f002
haru-voster <wabwireharoun2001@gmail.com>
2026-01-13 14:48:18 +0300

    purchase

--- purchaselist.php
+++ purchaselist.php
@@ -1,140 +1,112 @@
 <?php include 'lib/header.php'; ?>
 
-
 <?php
-//add invoice
-
+// Add invoice
 if (isset($_POST['addpurchase'])) {
-    if($inv->saveInvoice($_POST)){
+    if ($inv->saveInvoice($_POST)) {
         echo "<script>alert('Purchase Added Successfully');</script>";
-    }else{
-        echo "<script>alert('Purchase Added Failed!');</script>";
+    } else {
+        echo "<script>alert('Purchase Addition Failed!');</script>";
     }
 }
 
-
-
-//update invoice
-if ( isset($_POST['edit'])) {
-    if($inv->updateInvoice($_POST)){
+// Update invoice
+if (isset($_POST['edit'])) {
+    if ($inv->updateInvoice($_POST)) {
         echo "<script>alert('Purchase Updated Successfully');</script>";
-        echo "<script>window.loction='';</script>";
-
-    }else{
-        echo "<script>alert('Purchase Updated Failed!');</script>";
+        echo "<script>window.location='purchaselist.php';</script>"; // fixed typo 'loction'
+    } else {
+        echo "<script>alert('Purchase Update Failed!');</script>";
     }
-
 }
 
-//delete invoice
-if (isset($_GET['action']) && $_GET['action']=='delete') {
+// Delete invoice
+if (isset($_GET['action']) && $_GET['action'] == 'delete') {
     $serial = $_GET['serial'];
-    if($inv->deleteInvoice($serial, $_GET['invoice_id'])){
-         echo "<script>alert('Purchase Deleted Successfully');</script>";
-    }else{
-        echo "<script>alert('Purchase Deleted Failed!');</script>";
+    if ($inv->deleteInvoice($serial, $_GET['invoice_id'])) {
+        echo "<script>alert('Purchase Deleted Successfully');</script>";
+        echo "<script>window.location='purchaselist.php';</script>"; // reload after delete
+    } else {
+        echo "<script>alert('Purchase Deletion Failed!');</script>";
     }
-   
 }
 ?>
 
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <h1><i class="lnr lnr-list"></i> &nbsp;Purchase List</h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active"><a href="<?php echo BASE_URL; ?>">Dashboard</a></li>
-      </ol>
+        <h1><i class="lnr lnr-list"></i> &nbsp;Purchase List</h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active"><a href="<?php echo BASE_URL; ?>">Dashboard</a></li>
+        </ol>
     </section>
 
     <!-- Main content -->
     <section class="content">
-     <div class="row">
-       <div class="col-xs-12">
-         
-          <div class="box">
-            <div class="box-header">
-              <h3 class="box-title"></h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <table id="purchaselist" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
-                <thead>
-                <tr role="row">
-                  <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" width="10%">Serial</th>
-                  <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending"  width="10%">Invoice No</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending"  width="15%">Supplier</th>
-                  <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending"  width="10%">Quantity</th>
-                  <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending"  width="5%">Total</th>
-
-                  <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending"  width="10%">Date</th>
-
-                  
-                   <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending"  width="10%">Action</th>
-
-                </tr>
-                </thead>
-                <tbody>
-                        <?php
-                        $status = $inv->showInvoices();
-
-                        if ($status) {
-                            $i = 0;
-                            while ($result = $status->fetch_assoc()) {
-                                $i++;
-                                ?>
-                                <tr style="text-align: center;" id="rowid-<?php echo $result['serial']; ?>">
-                                    <td><?php echo $i; ?></td>
-                                    <td><?php echo $result['invoice_number']; ?></td>
-                                    <td style="text-align: left;"><?php echo $result['supplier_name']; ?></td>
-                                    <td><?php echo $result['quantity']; ?></td>
-                                    <td><?php echo $result['total']; ?></td>
-                                    <td><?php echo $help->formatDate($result['date'],'d-m-Y'); ?></td>
-
-                                    <?php if(Session::get('status') == 'admin'): ?>
-
-                                    <td>
-                                        <a href="viewpurchase.php?action=view&serial=<?php echo $result['serial'] ?>&invoice_id=<?php echo $result['invoice_number']; ?>" style="border-radius: 3px;" title="click to view" ><i class="fa fa-eye"></i>&nbsp;&nbsp;</a>
-                                        <a href="editpurchase.php?action=edit&serial=<?php echo $result['serial'] ?>&invoice_id=<?php echo $result['invoice_number']; ?>" style="border-radius: 3px;" title="click to edit" ><i class="fa fa-pencil-square-o"></i>&nbsp;&nbsp;</a>
-
-                                        <a href="printfiles/purchase/printpurchase.php?serial=<?php echo $result['serial'] ?>&invoice_id=<?php echo $result['invoice_number']; ?>" style="border-radius: 3px;" title="click to print" ><i class="fa fa-print"></i></a>
-
-                                        <a href="?action=delete&serial=<?php echo $result['serial'] ?>&invoice_id=<?php echo $result['invoice_number']; ?>" style="border-radius: 3px;" title="click to delete" onclick="return confirm('are you sure to delete?')" id="rowdelete" delid="<?php echo $result['serial']; ?>"><i class="lnr lnr-trash"></i></a>
-                                    </td>
-
-                                    <?php else:  ?>
-
-                                    <td>
-                                         <a href="viewpurchase.php?action=view&serial=<?php echo $result['serial'] ?>&invoice_id=<?php echo $result['invoice_number']; ?>" style="border-radius: 3px;" title="click to view" ><i class="fa fa-eye"></i>&nbsp;&nbsp;</a>
-                                    </td>
-                                   <?php endif; ?> 
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title">All Purchases</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <table id="purchaselist" class="table table-bordered table-hover dataTable">
+                            <thead>
+                                <tr style="text-align: center;">
+                                    <th width="10%">Serial</th>
+                                    <th width="10%">Invoice No</th>
+                                    <th width="15%">Supplier</th>
+                                    <th width="10%">Quantity</th>
+                                    <th width="5%">Total</th>
+                                    <th width="10%">Date</th>
+                                    <th width="10%">Action</th>
                                 </tr>
-
+                            </thead>
+                            <tbody>
                                 <?php
-                            }
-                        } else {
-                            ?>
+                                $status = $inv->showInvoices();
+                                if ($status) {
+                                    $i = 0;
+                                    while ($result = $status->fetch_assoc()) {
+                                        $i++;
+                                        ?>
+                                        <tr style="text-align: center;" id="rowid-<?php echo $result['serial']; ?>">
+                                            <td><?php echo $i; ?></td>
+                                            <td><?php echo $result['invoice_number']; ?></td>
+                                            <td style="text-align: left;"><?php echo $result['supplier_name']; ?></td>
+                                            <td><?php echo $result['quantity']; ?></td>
+                                            <td><?php echo $result['total']; ?></td>
+                                            <td><?php echo $help->formatDate($result['date'],'d-m-Y'); ?></td>
 
-                        <?php }
-                        ?>
-                        </tbody>
-              
-              </table>
+                                            <td>
+                                                <a href="viewpurchase.php?action=view&serial=<?php echo $result['serial']; ?>&invoice_id=<?php echo $result['invoice_number']; ?>" title="View"><i class="fa fa-eye"></i></a>
+                                                <?php if(Session::get('status') == 'admin'): ?>
+                                                    &nbsp;<a href="editpurchase.php?action=edit&serial=<?php echo $result['serial']; ?>&invoice_id=<?php echo $result['invoice_number']; ?>" title="Edit"><i class="fa fa-pencil-square-o"></i></a>
+                                                    &nbsp;<a href="printfiles/purchase/printpurchase.php?serial=<?php echo $result['serial']; ?>&invoice_id=<?php echo $result['invoice_number']; ?>" title="Print"><i class="fa fa-print"></i></a>
+                                                    &nbsp;<a href="?action=delete&serial=<?php echo $result['serial']; ?>&invoice_id=<?php echo $result['invoice_number']; ?>" title="Delete" onclick="return confirm('Are you sure to delete?')"><i class="lnr lnr-trash"></i></a>
+                                                <?php endif; ?>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                    }
+                                } else {
+                                    echo '<tr><td colspan="7" style="text-align:center;">No purchases found</td></tr>';
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
             </div>
-
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
         </div>
-      </div>
-
-      
     </section>
     <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
- <!-- footer -->
+</div>
+<!-- /.content-wrapper -->
 
- <?php include 'lib/footer.php'; ?>
+<?php include 'lib/footer.php'; ?>
