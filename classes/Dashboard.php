ea4582702561c09c759eb69d1a9230f8de171ca0
haru-voster <wabwireharoun2001@gmail.com>
2026-01-14 09:45:47 +0300

    login

--- classes/Dashboard.php
+++ classes/Dashboard.php
@@ -11,9 +11,7 @@ class Dashboard
     public function __construct()
     {
         $this->dbObj = new Database();
-
     }
-
 
     /*
     !--------------------------------------
@@ -26,23 +24,18 @@ class Dashboard
         $starting = date('Y-m-d') . " 00:00:00";
         $ending = date('Y-m-d') . " 23:59:59";
 
-        $query = "SELECT sum(sub_total) as 'subtotal' from tbl_sell ts where ts.date between '$starting' and '$ending'";
+        $query = "SELECT SUM(sub_total) as subtotal FROM tbl_sell ts WHERE ts.sell_date BETWEEN '$starting' AND '$ending'";
         $st = $this->dbObj->select($query);
         if ($st) {
             $subtotal = $st->fetch_object()->subtotal;
-            if ($subtotal > 0) {
-
-                return number_format((float)$subtotal, 2, '.', '');
-            } else {
-                return 0;
-            }
+            return ($subtotal > 0) ? number_format((float)$subtotal, 2, '.', '') : 0;
         }
+        return 0;
     }
-
 
     /*
     !--------------------------------------
-    !               Today Sales Amonut
+    !               Today Sales Count
     !-------------------------------------
     */
     public function todayMemo()
@@ -50,31 +43,28 @@ class Dashboard
         $starting = date('Y-m-d') . " 00:00:00";
         $ending = date('Y-m-d') . " 23:59:59";
 
-        $query = "SELECT count(sell_id) as 'totalsell' from tbl_sell where date between '$starting' and '$ending'";
+        $query = "SELECT COUNT(sell_id) as totalsell FROM tbl_sell WHERE sell_date BETWEEN '$starting' AND '$ending'";
         $st = $this->dbObj->select($query);
         if ($st) {
             return $st->fetch_object()->totalsell;
-        } else {
-            return 0;
         }
+        return 0;
     }
 
     /*
     !----------------------------------
-    !           Total Sales Amount
+    !           Total Sales Count
     !----------------------------------
     */
     public function totalMemo()
     {
-        $query = "SELECT count(sell_id) as 'totalsell' from tbl_sell";
+        $query = "SELECT COUNT(sell_id) as totalsell FROM tbl_sell";
         $st = $this->dbObj->select($query);
         if ($st) {
             return $st->fetch_object()->totalsell;
-        } else {
-            return 0;
         }
+        return 0;
     }
-
 
     /*
     !----------------------------------
@@ -83,109 +73,110 @@ class Dashboard
     */
     public function totalPurchase()
     {
-        $query = "SELECT count(serial) as 'totalinvoice' from tbl_invoice";
+        $query = "SELECT COUNT(serial) as totalinvoice FROM tbl_invoice";
         $st = $this->dbObj->select($query);
         if ($st) {
             return $st->fetch_object()->totalinvoice;
-        } else {
-            return 0;
         }
+        return 0;
     }
 
-
-    // Total Customer
+    /*
+    !----------------------------------
+    !         Total Customers
+    !----------------------------------
+    */
     public function todayCustomer()
     {
-        $query = "SELECT count(serial) as 'totalcustomer' from tbl_customer";
+        $query = "SELECT COUNT(customer_id) as totalcustomer FROM tbl_customer";
         $st = $this->dbObj->select($query);
         if ($st) {
             return $st->fetch_object()->totalcustomer;
-        } else {
-            return 0;
         }
-
+        return 0;
     }
 
-
-    /**
-     * Total Profit
-     * @return int|string|void
-     */
+    /*
+    !----------------------------------
+    !           Today Profit
+    !----------------------------------
+    */
     public function todayProfile()
     {
         $starting = date('Y-m-d') . " 00:00:00";
         $ending = date('Y-m-d') . " 23:59:59";
-        $query = "select sum(profit) as 'profit' from profit where profit.date between '$starting' and '$ending'";
 
+        $query = "SELECT SUM(profit) as profit FROM profit WHERE date BETWEEN '$starting' AND '$ending'";
         $stmt = $this->dbObj->link->query($query);
         if ($stmt) {
-            $data = $stmt->fetch_assoc();
-            $profit = $data['profit'];
-            if ($profit > 0) {
-                return number_format((float)$profit, 2, '.', '');
-            } else {
-                return 0;
-            }
+            $profit = $stmt->fetch_assoc()['profit'];
+            return ($profit > 0) ? number_format((float)$profit, 2, '.', '') : 0;
         }
-
+        return 0;
     }
 
-
-    // Total Products
+    /*
+    !----------------------------------
+    !           Total Products
+    !----------------------------------
+    */
     public function totalProducts()
     {
-
-        $query = "select count(product_id) as 'total' from tbl_product";
+        $query = "SELECT COUNT(product_id) as total FROM tbl_product";
         $st = $this->dbObj->link->query($query);
         if ($st) {
             $total = $st->fetch_object()->total;
-            if ($total > 0 || $total < 0) {
-
-                return $total;
-            } else {
-                return 0;
-            }
+            return ($total !== null) ? $total : 0;
         }
-
+        return 0;
     }
 
-
-    // Total Customer
+    /*
+    !----------------------------------
+    !           Total Customers
+    !----------------------------------
+    */
     public function totalCustomers()
     {
-
-        $query = "select count(customer_id) as 'total' from tbl_customer";
+        $query = "SELECT COUNT(customer_id) as total FROM tbl_customer";
         $st = $this->dbObj->link->query($query);
         if ($st) {
             $total = $st->fetch_object()->total;
-            if ($total > 0 || $total < 0) {
-
-                return $total;
-            } else {
-                return 0;
-            }
+            return ($total !== null) ? $total : 0;
         }
-
+        return 0;
     }
 
-
-    // Total Supplier
+    /*
+    !----------------------------------
+    !           Total Suppliers
+    !----------------------------------
+    */
     public function totalSuppliers()
     {
-
-        $query = "select count(supplier_id) as 'total' from tbl_supplier";
+        $query = "SELECT COUNT(supplier_id) as total FROM tbl_supplier";
         $st = $this->dbObj->link->query($query);
         if ($st) {
             $total = $st->fetch_object()->total;
-            if ($total > 0 || $total < 0) {
-
-                return $total;
-            } else {
-                return 0;
-            }
+            return ($total !== null) ? $total : 0;
         }
-
+        return 0;
     }
 
+    /*
+    !----------------------------------
+    !           Total Due
+    !----------------------------------
+    */
+    public function totalDue()
+    {
+        $query = "SELECT SUM(total_due) as totaldue FROM tbl_sell";
+        $st = $this->dbObj->link->query($query);
+        if ($st) {
+            $total = $st->fetch_object()->totaldue;
+            return ($total !== null) ? number_format((float)$total, 2, '.', '') : 0;
+        }
+        return 0;
+    }
 
 }
--- login.php
+++ login.php
@@ -1,9 +1,21 @@
 <?php
 ob_start();
-include 'classes/Session.php';
-include_once 'classes/Login.php';
-Session::checkLogin();
+require_once 'classes/Session.php';
+require_once 'classes/Login.php';
+
+Session::init();
+
+if (Session::get('login') === true) {
+    header("Location: dashboard.php");
+    exit;
+}
+
 $log = new Login();
+$message = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $message = $log->login($_POST);
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,79 +23,57 @@ $log = new Login();
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Explore POS | Log in</title>
-  <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
+
   <link rel="stylesheet" href="assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
   <link rel="stylesheet" href="assets/bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
   <link rel="stylesheet" href="assets/bower_components/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
   <link rel="stylesheet" href="assets/dist/css/AdminLTE.min.css">
-  <!-- iCheck -->
   <link rel="stylesheet" href="assets/plugins/iCheck/square/blue.css">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-  <!-- Google Font -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700">
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>Panel</a>
+    <a href="#"><b>Admin</b>Panel</a>
   </div>
-  <!-- /.login-logo -->
   <div class="login-box-body">
-    <?php
-      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-          $status = $log->login($_POST);
-      }
-      ?>
-    <form action="" method="post">
+
+    <?php if (!empty($message)) : ?>
+      <div class="alert alert-danger"><?php echo $message; ?></div>
+    <?php endif; ?>
+
+    <form action="" method="post" autocomplete="off">
       <div class="form-group has-feedback">
-        <input type="username" name="username" class="form-control" placeholder="Username">
+        <input type="text" name="username" class="form-control" placeholder="Username" required>
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" name="password" class="form-control" placeholder="Password">
+        <input type="password" name="password" class="form-control" placeholder="Password" required>
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
-       
-        <!-- /.col -->
         <div class="col-xs-4">
           <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
         </div>
-        <!-- /.col -->
       </div>
     </form>
-    <br>Technical Support | 
-    <a href="http://www.exploreit.com.bd/">exploreit.com.bd</a><br>
+
+    <br>
+    <small>Technical Support | <a href="http://www.exploreit.com.bd/">exploreit.com.bd</a></small>
 
   </div>
-  <!-- /.login-box-body -->
 </div>
-<!-- /.login-box -->
 
-<!-- jQuery 3 -->
 <script src="assets/bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
 <script src="assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- iCheck -->
 <script src="assets/plugins/iCheck/icheck.min.js"></script>
 <script>
   $(function () {
     $('input').iCheck({
       checkboxClass: 'icheckbox_square-blue',
       radioClass: 'iradio_square-blue',
-      increaseArea: '20%' /* optional */
+      increaseArea: '20%'
     });
   });
 </script>
